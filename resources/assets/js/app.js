024886629cc233167aadf0b9097c566c4b4f9631
ferretdayo <ferretdayo@hotmail.co.jp>
2017-05-04 09:52:08 +0900

    基本の実装終了

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,5 +18,90 @@ window.Vue = require('vue');
 Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: function() {
+        return {
+            code: "",
+            selected: [],
+            foods: [
+                {id: 1, name: "白ご飯", img: "http://localhost:8000/img/gohan.jpg"},
+                {id: 2, name: "鶏の唐揚げ", img: "http://localhost:8000/img/karaage.jpg"},
+                {id: 3, name: "シーザーサラダ", img: "http://localhost:8000/img/sarada.jpg"},
+                {id: 1, name: "白ご飯", img: "http://localhost:8000/img/gohan.jpg"},
+                {id: 2, name: "鶏の唐揚げ", img: "http://localhost:8000/img/karaage.jpg"},
+                {id: 3, name: "シーザーサラダ", img: "http://localhost:8000/img/sarada.jpg"},
+                {id: 1, name: "白ご飯", img: "http://localhost:8000/img/gohan.jpg"},
+                {id: 2, name: "鶏の唐揚げ", img: "http://localhost:8000/img/karaage.jpg"},
+                {id: 3, name: "シーザーサラダ", img: "http://localhost:8000/img/sarada.jpg"},
+            ],
+            drinks: [
+                {id: 1, name: "お茶", img: "http://localhost:8000/img/ocha.jpg"},
+                {id: 2, name: "ビール", img: "http://localhost:8000/img/beer.jpg"},
+                {id: 3, name: "コークハイ", img: "http://localhost:8000/img/sake.jpg"},
+            ],
+            desserts: [
+                {id: 1, name: "パンケーキ", img: "http://localhost:8000/img/pancake.jpg"},
+                {id: 2, name: "バニラアイス", img: "http://localhost:8000/img/ice.jpg"},
+                {id: 3, name: "パフェ", img: "http://localhost:8000/img/pafe.jpg"},
+            ]
+        }
+    },
+    computed: {
+        // 注文のすべての数の計算
+        allOrder: function() {
+            let sum = 0;
+            for(let i = 0; i < this.selected.length; i++) {
+                sum += this.selected[i].cnt
+            }
+            return sum;
+        },
+        // キャラクター or アイテムのコードの生成
+        codeGenerate: function() {
+            let sum = 0;
+            for(let i = 0; i < this.selected.length; i++) {
+                sum += this.selected[i].cnt
+            }
+            if (sum < 4) {
+                return "hoge"
+            }
+            if (sum < 8) {
+                return "foo!!"
+            }
+            if (sum < 10) {
+                return "最高"
+            }
+        }
+    },
+    methods: {
+        // 注文商品の数を追加するメソッド
+        add: function(name) {
+            let filter = []
+            filter = this.selected.filter((value) => {
+                if (value.name === name) return true;
+                else return false;
+            })
+            // すでにある場合
+            if (filter.length > 0) {
+                for(let i = 0; i < this.selected.length; i++) {
+                    if (this.selected[i].name === name) {
+                        this.selected[i].cnt++;
+                    }
+                }
+            } else {
+                this.selected.push({name: name, cnt: 1})
+            }
+        },
+        // 注文商品の数を減らすメソッド
+        sub: function(name) {
+            for(let i = 0; i < this.selected.length; i++) {
+                if (this.selected[i].name === name) {
+                    this.selected[i].cnt--;
+                }
+            }
+            this.selected = this.selected.filter((value) => {
+                if (value.cnt > 0) return true;
+                else return false;
+            })
+        },
+    }
 });
---
+++ resources/views/menu/index.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+<ul class="nav nav-tabs nav-justified">
+    <li role="menu" class="active"><a href="#food" data-toggle="tab"><span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span> フード</a></li>
+    <li role="menu"><a href="#drink" data-toggle="tab"><span class="glyphicon glyphicon-baby-formula" aria-hidden="true"></span> ドリンク</a></li>
+    <li role="menu"><a href="#dessert" data-toggle="tab"><span class="glyphicon glyphicon-ice-lolly-tasted" aria-hidden="true"></span> デザート</a></li>
+</ul>
+<div class="row">
+    <div class="col-md-8 col-sm-9 col-lg-9">
+        <!--タブのコンテンツ部分-->
+        <div class="tab-content">
+            <div id="food" class="tab-pane active">
+                <div class="content">
+                    <div class="thumbnail" v-for="food in foods">
+                        <img v-bind:src="food.img" alt="..." width="200" height="200">
+                        <div class="caption">
+                            <h3 class="title">@{{food.name}}</h3>
+                            <p><a href="#" class="btn btn-success btn-block add" role="button" v-on:click="add(food.name)">追加</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="drink" class="tab-pane">
+                <div class="content">
+                    <div class="thumbnail" v-for="drink in drinks">
+                        <img v-bind:src="drink.img" alt="..." width="200" height="200">
+                        <div class="caption">
+                            <h3 class="title">@{{drink.name}}</h3>
+                            <p><a href="#" class="btn btn-success btn-block add" role="button" v-on:click="add(drink.name)">追加</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="dessert" class="tab-pane">
+                <div class="content">
+                    <div class="thumbnail" v-for="dessert in desserts">
+                        <img v-bind:src="dessert.img" alt="..." width="200" height="200">
+                        <div class="caption">
+                            <h3 class="title">@{{dessert.name}}</h3>
+                            <p><a href="#" class="btn btn-success btn-block add" role="button" v-on:click="add(dessert.name)">追加</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-3 col-lg-3 menu-list">
+        <h2 class="title">注文リスト</h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <td></td>
+                    <td>商品名</td>
+                    <td>注文数</td>
+                    <td></td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="select in selected">
+                    <td><button class="btn btn-primary" v-on:click="sub(select.name)"> - </button></td>
+                    <td>@{{select.name}}</td>
+                    <td>@{{select.cnt}}</td>
+                    <td><button class="btn btn-primary" v-on:click="add(select.name)"> + </button></td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="order-cnt">
+            <span class="title">現在の注文数</span><span class="order">@{{allOrder}}</span>
+        </div>
+        <div class="submit">
+            <button class="btn btn-warning btn-block" data-toggle="modal" data-target="#myModal">注文</button>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">注文しました:D</h4>
+      </div>
+      <div class="modal-body">
+        QRコード？
+        @{{codeGenerate}}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
